ea12afb84b3d7064a9130df763ddf6bc25cda27c
Danish Khan <danishkhan@danishkhan.org>
2011-04-12 15:12:11 -0700

    ability to restart gollum server properly

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,7 +1,8 @@
+run "sudo lsof -n -i 'TCP:80' -Fp | cut -c 2-6 | sudo xargs kill -9"
+
 if File.directory?("#{current_path}/_site")
   run "cd #{current_path} && rm -rf _site"
 end
 
-sudo "lsof -n -i 'TCP:80' -Fp"
 run "cd #{current_path} && bundle exec gollum-site generate"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
