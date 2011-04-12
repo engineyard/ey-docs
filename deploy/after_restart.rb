72346d93d059176fe3b39409de0f8c5da08ac4c6
Danish Khan <danishkhan@danishkhan.org>
2011-04-11 18:36:06 -0700

    working on deploy hooks to make deployment easier

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,5 +2,6 @@ if File.directory?("#{current_path}/_site")
   run "cd #{current_path} && rm -rf _site"
 end
 
+sudo "lsof -n -i 'TCP:80' -Fp"
 run "cd #{current_path} && bundle exec gollum-site generate"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
