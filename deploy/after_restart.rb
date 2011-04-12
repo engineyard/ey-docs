e4fa19c4d3169646f0140b00713c3c0175f80c64
Danish Khan <danishkhan@danishkhan.org>
2011-04-11 18:07:21 -0700

    added if statement for deleteing site folder

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,6 @@
-# run "cd #{current_path} && rm -rf _site"
+if File.directory?("#{current_path}/_site")
+  run "cd #{current_path} && rm -rf _site"
+end
+
 run "cd #{current_path} && bundle exec gollum-site generate"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
