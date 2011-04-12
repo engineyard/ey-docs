5d5ee483e3db9090c0ae12b97d446e2dd65a620d
Danish Khan <danishkhan@danishkhan.org>
2011-04-11 17:57:38 -0700

    Commented out rm line

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,3 @@
-run "cd #{current_path} && rm -rf _site"
+# run "cd #{current_path} && rm -rf _site"
 run "cd #{current_path} && bundle exec gollum-site generate"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
