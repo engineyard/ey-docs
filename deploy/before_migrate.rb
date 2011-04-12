b352fe0dc61ed51214a351bc4f4f74d163dd3328
Danish Khan <danishkhan@danishkhan.org>
2011-04-12 15:39:09 -0700

    changed site folder removal to root permissions

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,8 @@
 sudo "lsof -n -i 'TCP:80' -Fp | cut -c 2-6 | xargs kill -9"
 
-if File.directory?("#{current_path}/_site")
-  run "cd #{current_path} && rm -rf _site"
+if (File.directory?("#{current_path}/_site") == true)
+  sudo "cd #{current_path} && rm -rf _site"
 end
 
-sudo "cd #{current_path} && bundle exec gollum-site generate"
+run "cd #{current_path} && bundle exec gollum-site generate"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
