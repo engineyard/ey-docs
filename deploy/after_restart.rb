25c44ce91a34cfef279587aee938323a89512bab
Danish Khan <danishkhan@danishkhan.org>
2011-04-26 15:17:39 -0700

    Updated deploy hook. Fixes #9

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,5 +2,5 @@ sudo "lsof -n -i 'TCP:80' -Fp | cut -c 2-6 | xargs kill -9"
 
 sudo "cd #{current_path} && rm -rf _site"
 
-run "cd #{current_path} && bundle exec gollum-site generate"
+run "cd #{current_path} && bundle exec gollum-site generate --allow_elements iframe --allow_attributes src --allow_protocols http,https"
 sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
