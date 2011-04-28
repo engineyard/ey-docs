970c0021887446931a25375b42e84df5b38fe330
Danish Khan <danishkhan@danishkhan.org>
2011-04-27 17:29:37 -0700

    remove typo in deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,4 +3,4 @@ sudo "/etc/init.d/nginx stop"
 sudo "cd #{current_path} && rm -rf _site"
 
 run "cd #{current_path} && bundle exec gollum-site generate --allow_elements iframe --allow_attributes src --allow_protocols http,https"
-sudo "c/etc/init.d/nginx start"
+sudo "/etc/init.d/nginx start"
