93bf5f583f06fbc207b1481847ad328d98f052fa
Danish Khan <danishkhan@danishkhan.org>
2011-04-27 17:24:25 -0700

    Added changes for nginx to deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,6 +1,6 @@
-sudo "lsof -n -i 'TCP:80' -Fp | cut -c 2-6 | xargs kill -9"
+sudo "/etc/init.d/nginx stop"
 
 sudo "cd #{current_path} && rm -rf _site"
 
 run "cd #{current_path} && bundle exec gollum-site generate --allow_elements iframe --allow_attributes src --allow_protocols http,https"
-sudo "cd #{current_path} && bundle exec gollum-site serve --port 80 &"
+sudo "c/etc/init.d/nginx start"
