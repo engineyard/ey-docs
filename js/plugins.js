1ca54ef9840d975d127b0361b3ff006f36e544ad
Gerlando Piro <gerlando@gmail.com>
2011-09-15 14:56:27 -0700

    fixed third level sidebar highlighting

--- js/plugins.js
+++ js/plugins.js
@@ -2,32 +2,56 @@
 (function($){
 
   $(document).ready(function(){
+    
+    var setselected = function($obj) {
+      $obj.find('> ul > li').each(function() {
+        var $deepli = $(this);
+        if ($deepli.has("a[href=\"" + document.location.pathname + "\"]").length != 0) {
+          $deepli.addClass('selected');
+          initnav($deepli);
+        }
+      });
+    };
+    
+    var showchildren = function($obj) {
+      $obj.removeClass('collapsed');
+      $obj.find("ul").show();
+    }
+    
+    var initnav = function($obj) {
+      // collapse and hide children
+      $obj.addClass("collapsed"); 
+      $obj.find("ul").hide();
+      addarrow($obj);
+      expandnav($obj);
+    }
+    
+    var addarrow = function($obj) {
+      // Add arrow if li has children
+      if ($obj.has("ul").length) { $obj.find('> a').addClass('arrow'); }
+    }
+    
+    var expandnav = function($obj) {
+      $link = $obj.find('> a');
+      // expand and select if link is active
+      if ($link.attr('href') == document.location.pathname) {
+        $obj.addClass('selected expanded');
+        selectandshowchildren($obj,$link)
+      // expand if child link is active
+      } else if ($obj.has("a[href=\"" + document.location.pathname + "\"]").length != 0) {
+        $obj.addClass('selected parent expanded');
+        selectandshowchildren($obj,$link)
+      }
+    }
+    
+    var selectandshowchildren = function($obj,$link) {
+      $link.addClass('selected');
+      showchildren($obj);
+      setselected($obj);
+    }
+    
     $(".subnav > ul > li").each(function(){
-      
-      var li = $(this);
-      var firstlink = $(this).find('> a');
-      
-      if (li.has("ul").length) {
-        firstlink.addClass('arrow');
-        $(this).find('> ul > li').each(function() {
-          var deepli = $(this);
-          if (deepli.has("a[href=\"" + document.location.pathname + "\"]").length != 0) {
-            deepli.addClass('selected');
-          }
-        });
-      }
-      
-      if (firstlink.attr('href') == document.location.pathname) {
-        li.addClass('selected expanded');
-        firstlink.addClass('selected');
-      } else if (li.has("a[href=\"" + document.location.pathname + "\"]").length != 0) {
-        li.addClass('selected parent expanded');
-        firstlink.addClass('selected');
-      } else {
-        li.addClass("collapsed"); 
-        li.find("ul").hide();
-      }
-      
+      initnav($(this));
     });
   });                                            
 })(this.jQuery);
