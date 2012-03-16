526d811abca4e42340c5fb9f95f8deeff28fb958
Danish Khan <dk@danishkhan.org>
2012-03-16 13:30:55 -0700

    change

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,3 @@
-#sudo "cd #{current_path} && rm -rf _site"
-#run "cd #{current_path} && bundle exec gollum-site generate --allow_elements iframe --allow_attributes src --allow_protocols http,https"
-#sudo "/etc/init.d/nginx reload"
+sudo "cd #{current_path} && rm -rf _site"
+run "cd #{current_path} && bundle exec gollum-site generate --allow_elements iframe --allow_attributes src --allow_protocols http,https"
+sudo "/etc/init.d/nginx reload"
